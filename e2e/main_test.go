46d19a5fa9172c802ba99d2c9ccbf29d1f8bca8e
mashiike <ikeda-masashi@kayac.com>
2022-08-17 18:24:46 +0900

    to table driven test

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -21,6 +21,87 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type serverTestCase struct {
+	Name     string
+	TestFunc func(t *testing.T, ctx context.Context, conn *pgx.Conn)
+}
+
+var serverTestCases = []serverTestCase{
+	{
+		Name: "fetch example.fuga with cursor",
+		TestFunc: func(t *testing.T, ctx context.Context, conn *pgx.Conn) {
+			_, err := conn.Exec(ctx, "BEGIN;")
+			require.NoError(t, err)
+			_, err = conn.Exec(ctx, "SET statement_timeout = 18000;")
+			log.Printf("[notice] test exec set")
+			require.NoError(t, err)
+			log.Printf("[notice] test exec declare")
+			_, err = conn.Exec(ctx, "DECLARE cursor_test_1234 NO SCROLL CURSOR FOR SELECT ymd,name,value FROM example.fuga LIMIT 100;")
+			require.NoError(t, err)
+			log.Printf("[notice] test query fetch")
+			rows, err := conn.Query(ctx, "FETCH 5 IN cursor_test_1234;")
+			require.NoError(t, err)
+			actual := make([][]interface{}, 0)
+			log.Printf("[notice] test query fetch read rows")
+			for rows.Next() {
+				values, err := rows.Values()
+				log.Printf("[notice] test rows=[%v]", values)
+				require.NoError(t, err)
+				actual = append(actual, values)
+			}
+			require.NoError(t, rows.Err())
+			_, err = conn.Exec(ctx, "CLOSE cursor_test_1234;")
+			require.NoError(t, err)
+			_, err = conn.Exec(ctx, "COMMIT;")
+			require.NoError(t, err)
+			expected := [][]interface{}{
+				{time.Date(2022, 01, 01, 0, 0, 0, 0, time.UTC), "正月", int32(0)},
+				{time.Date(2022, 01, 02, 0, 0, 0, 0, time.UTC), "なにもない日", int32(1)},
+			}
+			require.EqualValues(t, expected, actual)
+		},
+	},
+}
+
+func (c *serverTestCase) Run(t *testing.T, ctx context.Context, cfg *psqlfront.Config, addr string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
+	conn, ok := func() (*pgx.Conn, bool) {
+		p := backoff.Exponential(
+			backoff.WithMinInterval(200*time.Millisecond),
+			backoff.WithMaxInterval(1*time.Second),
+			backoff.WithJitterFactor(0.05),
+			backoff.WithMaxRetries(6),
+		)
+		retryer := p.Start(ctx)
+		dsn := fmt.Sprintf(
+			"postgres://%s:%s@%s/%s?sslmode=prefer",
+			cfg.CacheDatabase.Username,
+			cfg.CacheDatabase.Password,
+			addr,
+			cfg.CacheDatabase.Database,
+		)
+		t.Log("connect:", dsn)
+		for backoff.Continue(retryer) {
+			conn, err := pgx.Connect(ctx, dsn)
+			if err == nil {
+				return conn, true
+			}
+			t.Log(err)
+		}
+		return nil, false
+	}()
+	if !ok {
+		t.Fatal("can not connect")
+	}
+	t.Run(c.Name, func(t *testing.T) {
+		c.TestFunc(t, ctx, conn)
+	})
+
+	conn.Close(ctx)
+	cancel()
+}
+
 func TestServer(t *testing.T) {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/fuga", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -46,7 +127,7 @@ func TestServer(t *testing.T) {
 	defer listener.Close()
 	server, err := psqlfront.New(context.Background(), cfg)
 	require.NoError(t, err)
-	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 
 	var wg sync.WaitGroup
@@ -58,65 +139,9 @@ func TestServer(t *testing.T) {
 		require.NoError(t, err)
 	}()
 
-	conn, ok := func() (*pgx.Conn, bool) {
-		p := backoff.Exponential(
-			backoff.WithMinInterval(200*time.Millisecond),
-			backoff.WithMaxInterval(1*time.Second),
-			backoff.WithJitterFactor(0.05),
-			backoff.WithMaxRetries(6),
-		)
-		retryer := p.Start(ctx)
-		dsn := fmt.Sprintf(
-			"postgres://%s:%s@%s/%s?sslmode=prefer",
-			cfg.CacheDatabase.Username,
-			cfg.CacheDatabase.Password,
-			listener.Addr(),
-			cfg.CacheDatabase.Database,
-		)
-		t.Log("connect:", dsn)
-		for backoff.Continue(retryer) {
-			conn, err := pgx.Connect(ctx, dsn)
-			if err == nil {
-				return conn, true
-			}
-			t.Log(err)
-		}
-		return nil, false
-	}()
-	if !ok {
-		t.Fatal(err)
+	for _, c := range serverTestCases {
+		c.Run(t, ctx, cfg, listener.Addr().String())
 	}
-
-	_, err = conn.Exec(ctx, "BEGIN;")
-	require.NoError(t, err)
-	_, err = conn.Exec(ctx, "SET statement_timeout = 18000;")
-	log.Printf("[notice] test exec set")
-	require.NoError(t, err)
-	log.Printf("[notice] test exec declare")
-	_, err = conn.Exec(ctx, "DECLARE cursor_test_1234 NO SCROLL CURSOR FOR SELECT ymd,name,value FROM example.fuga LIMIT 100;")
-	require.NoError(t, err)
-	log.Printf("[notice] test query fetch")
-	rows, err := conn.Query(ctx, "FETCH 5 IN cursor_test_1234;")
-	require.NoError(t, err)
-	actual := make([][]interface{}, 0)
-	log.Printf("[notice] test query fetch read rows")
-	for rows.Next() {
-		values, err := rows.Values()
-		log.Printf("[notice] test rows=[%v]", values)
-		require.NoError(t, err)
-		actual = append(actual, values)
-	}
-	require.NoError(t, rows.Err())
-	_, err = conn.Exec(ctx, "CLOSE cursor_test_1234;")
-	require.NoError(t, err)
-	_, err = conn.Exec(ctx, "COMMIT;")
-	require.NoError(t, err)
-	expected := [][]interface{}{
-		{time.Date(2022, 01, 01, 0, 0, 0, 0, time.UTC), "正月", int32(0)},
-		{time.Date(2022, 01, 02, 0, 0, 0, 0, time.UTC), "なにもない日", int32(1)},
-	}
-	require.EqualValues(t, expected, actual)
-	conn.Close(ctx)
 	cancel()
 	wg.Wait()
 }
